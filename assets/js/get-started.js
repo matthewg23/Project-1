9f474e063f05ed6f54d10c2313a049ca00dddf2b
Huy Nguyen <huy.m.nguyen@live.com>
2019-07-13 12:26:19 -0700

    Added eventbrite api key. Centered button in get-started.html

---
+++ assets/js/get-started.js
@@ -0,0 +1,5 @@
+// eventbrite api key
+// api main key 7WVZTR2WHS7TQ6WZ4L
+// private api key 4ZB37NDCRJ564RCOB2UD
+// public api key WU7KU6HKP6T3INTQLZR2
+// https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=YOUR_APP_KEY&redirect_uri=YOUR_URL
