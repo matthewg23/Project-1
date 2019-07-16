f46fec704b6d63476ff4930a8d03351759b9a20f
Huy Nguyen <huy.m.nguyen@live.com>
2019-07-15 19:25:34 -0700

    Add query link for eventbrite

--- assets/js/get-started.js
+++ assets/js/get-started.js
@@ -6,5 +6,15 @@
 // Getting data from Eventbrite API
 $(document).on('click', '#button-event', function() {
     var event = $(this).attr('data-event');  /////// Add proper for 'data-event'
-    var queryURL = ''
-})
+    var queryURL = 'https://www.eventbrite.com/oauth/authorize?q=' + event + '&response_type=token&client_id=WU7KU6HKP6T3INTQLZR2&redirect_uri=YOUR_URL&location.within10mi'
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var
+        }
+    });
+});
