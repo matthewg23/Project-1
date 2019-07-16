132d6cb157fb8ac39df3bb0edbd4a51a51794491
Huy Nguyen <huy.m.nguyen@live.com>
2019-07-15 18:53:59 -0700

    Added eventbrite api in get-started.js

--- assets/js/get-started.js
+++ assets/js/get-started.js
@@ -1,5 +1,10 @@
 // eventbrite api key
 // api main key 7WVZTR2WHS7TQ6WZ4L
-// private api key 4ZB37NDCRJ564RCOB2UD
 // public api key WU7KU6HKP6T3INTQLZR2
 // https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=YOUR_APP_KEY&redirect_uri=YOUR_URL
+
+// Getting data from Eventbrite API
+$(document).on('click', '#button-event', function() {
+    var event = $(this).attr('data-event');  /////// Add proper for 'data-event'
+    var queryURL = ''
+})
