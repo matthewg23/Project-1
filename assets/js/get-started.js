6c3117f5dfa888d70b440875744c971399a72afe
Huy Nguyen <huy.m.nguyen@live.com>
2019-07-15 19:28:21 -0700

    Removed comment with api keys

--- assets/js/get-started.js
+++ assets/js/get-started.js
@@ -1,8 +1,3 @@
-// eventbrite api key
-// api main key 7WVZTR2WHS7TQ6WZ4L
-// public api key WU7KU6HKP6T3INTQLZR2
-// https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=YOUR_APP_KEY&redirect_uri=YOUR_URL
-
 // Getting data from Eventbrite API
 $(document).on('click', '#button-event', function() {
     var event = $(this).attr('data-event');  /////// Add proper for 'data-event'
